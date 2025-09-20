59c7faadc58a7c0d63a029c494ebaa086d9efb2e
Enrico Meluziis <enricomlz@hotmail.it>
2025-09-20 17:28:13 +0200

    text button modified

--- resources/views/blabs/index.blade.php
+++ resources/views/blabs/index.blade.php
@@ -4,12 +4,10 @@
             @csrf
             <textarea name="message" id="" cols="30" rows="10"
                 placeholder="{{ __('A cosa stai pensando?') }}"
-                class="block w-full rounded border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-              {{ old('message') }}
-            </textarea>
+                class="block w-full rounded border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">{{ old('message') }}</textarea>
             <x-input-error :messages="$errors->get('message')" class="mt-2"></x-input-error>
             <x-primary-button
-                class="mt-2">{{ __('Pubblica blab') }}</x-primary-button>
+                class="mt-2">{{ __('Pubblica') }}</x-primary-button>
         </form>
     </div>
 </x-app-layout>
